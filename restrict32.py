ea7c2ca5cf3197e0cb67d57b917809dedc53532e
robertomath <robertonicolaides@hotmail.co.uk>
2015-06-17 16:06:46 +0100

    A couple of questions on this code.

--- restrict32.py
+++ restrict32.py
@@ -1,5 +1,7 @@
 from tiling2 import Tiling2
 from vector2 import Vector2
+#I'm struggling to understand what this function is doing. Can you enlighten me by answering these questions I've added?
+#Are we trying to take a 3D tiling and 2D Tiling  and produce a 2D tile to represent the intersection with distances between verticies preserved from 3D to 2D?
 
 
 def restrict32(t):
@@ -9,7 +11,7 @@ def restrict32(t):
     For now, it complains if any vertices lie in the intersection
     (this should be a measure-zero event...)
     """
-
+    
     for (v,x) in t.vertices.iteritems():
         if v.z == 0:
             raise ValueError("Vertex %s lies in plane z=0"%(v,))
@@ -22,7 +24,7 @@ def restrict32(t):
         if v1.z < 0 < v2.z:
             u1 = Vector2(v1.x, v1.y)
             u2 = Vector2(v2.x, v2.y)
-            newv[e] = ((u1*(v2.z) - u2*(v1.z))/(v2.z - v1.z), x)
+            newv[e] = ((u1*(v2.z) - u2*(v1.z))/(v2.z - v1.z), x) #I am confused at what this line is doing geometrically. Please can you explain?
 
     newe = {}
     for (f,x) in t.faces.iteritems():
