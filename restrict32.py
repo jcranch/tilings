64ba6d63feab642f2f55b59c53708ddc1d85b0aa
James Cranch <J.D.Cranch@sheffield.ac.uk>
2015-06-17 16:55:53 +0100

    Better documentation for restrict32.

--- restrict32.py
+++ restrict32.py
@@ -1,15 +1,24 @@
 from tiling2 import Tiling2
 from vector2 import Vector2
-#I'm struggling to understand what this function is doing. Can you enlighten me by answering these questions I've added?
-#Are we trying to take a 3D tiling and 2D Tiling  and produce a 2D tile to represent the intersection with distances between verticies preserved from 3D to 2D?
 
 
 def restrict32(t):
     """
-    Restrict a Tiling3 to a Tiling2 (by taking z coordinate = 0).
+    Given a Tiling3, produce the Tiling2 corresponding to the
+    cross-section consisting of the plane with z coordinate = 0.
 
-    For now, it complains if any vertices lie in the intersection
-    (this should be a measure-zero event...)
+    For example, if t is a cubic tiling, translated a little, then the
+    result will be a square tiling.
+
+    If you want a different plane cross-section, you've got to rotate
+    and translate the tiling first.
+
+    For now, it complains if any vertices have z = 0. Of course, this
+    should be a measure-zero event, and can be avoided by small
+    translations. This makes the algorithm much simpler: edges in the
+    input produce vertices in the output; faces in the input produce
+    edges in the output, and volumes in the input produce faces in the
+    output.
     """
     
     for (v,x) in t.vertices.iteritems():
@@ -22,9 +31,12 @@ def restrict32(t):
         if v2.z < 0 < v1.z:
             (v1,v2) = (v2,v1)
         if v1.z < 0 < v2.z:
+            # If the edge passes through the plane z=0, then the
+            # result will contain a vertex lying at the point of
+            # intersection.
             u1 = Vector2(v1.x, v1.y)
             u2 = Vector2(v2.x, v2.y)
-            newv[e] = ((u1*(v2.z) - u2*(v1.z))/(v2.z - v1.z), x) #I am confused at what this line is doing geometrically. Please can you explain?
+            newv[e] = ((u1*(v2.z) - u2*(v1.z))/(v2.z - v1.z), x)
 
     newe = {}
     for (f,x) in t.faces.iteritems():
