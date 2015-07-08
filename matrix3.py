5a3a625835577c947c5f5cfaf26e8d251d3d71bf
James Cranch <j.d.cranch@sheffield.ac.uk>
2015-07-08 11:48:24 +0100

    Random orthogonal 3x3 matrices, and some helpful methods.

--- matrix3.py
+++ matrix3.py
@@ -1,6 +1,6 @@
 from math import sin, cos
 
-from vector3 import Vector3
+from vector3 import Vector3, random_norm1
 
 
 class Matrix3():
@@ -9,6 +9,9 @@ class Matrix3():
         ((a,b,c),(d,e,f),(g,h,i)) = l
         self.array = ((a,b,c),(d,e,f),(g,h,i))
 
+    def __repr__(self):
+        return "Matrix3([[%f, %f, %f], [%f, %f, %f], [%f, %f, %f]])"%(tuple(self[(i,j)] for i in [1,2,3] for j in [1,2,3]))
+        
     def __getitem__(self, (r, s)):
         return self.array[r-1][s-1]
 
@@ -24,6 +27,13 @@ class Matrix3():
         [a,b,c] = [sum(self[(i,j)]*other[j] for j in [1,2,3]) for i in [1,2,3]]
         return Vector3(a,b,c)
 
+    def row(self,i):
+        return Vector3(self[(1,i)], self[(2,i)], self[(3,i)])
+
+    def column(self,j):
+        return Vector3(self[(j,1)], self[(j,2)], self[(j,3)])
+
+
 
 def rotate_x(theta):
     return Matrix3([[1,0,0],[0,cos(theta),sin(theta)],[0,-sin(theta),cos(theta)]])
@@ -34,3 +44,12 @@ def rotate_y(theta):
 def rotate_z(theta):
     return Matrix3([[cos(theta),sin(theta),0],[-sin(theta),cos(theta),0],[0,0,1]])
 
+def random_orthogonal():
+    u = random_norm1()
+    v0 = random_norm1()
+    v1 = v0-u*(u.dot(v0))
+    v = v1/(v1.norm())
+    w0 = random_norm1()
+    w1 = w0-u*(u.dot(w0))-v*(v.dot(w0))
+    w = w1/(w1.norm())
+    return Matrix3([u,v,w])
--- vector3.py
+++ vector3.py
@@ -1,4 +1,5 @@
 from math import sqrt
+from random import random
 
 
 class Vector3():
@@ -36,8 +37,31 @@ class Vector3():
     def __div__(self, s):
         return Vector3(self.x/s, self.y/s, self.z/s)
 
+    def dot(self, other):
+        return self.x*other.x + self.y*other.y + self.z*other.z
+    
     def norm(self):
         return sqrt(self.x*self.x + self.y*self.y + self.z*self.z)
     
     def distance(self, other):
         return (self-other).norm()
+
+    def __iter__(self):
+        yield self.x
+        yield self.y
+        yield self.z
+
+
+def random_norm1():
+    """
+    A random vector on the unit sphere.
+    """
+    while True:
+        # A point uniformly distributed in the box [-1,1] * [-1,1] * [-1,1]
+        v = Vector3(random()*2-1, random()*2-1, random()*2-1)
+        n = v.norm()
+        # We restrict to points inside the unit sphere so as to get
+        # points of uniformly distributed direction, and reject points
+        # with tiny norm because we're worried about dividing by zero.
+        if n < 1.0 and n > 0.01:
+            return v/n
