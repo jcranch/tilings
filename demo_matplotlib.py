0e9f8bbd3fa4bf3f9f852c9499490f715fa0b4d9
robertomath <robertonicolaides@hotmail.co.uk>
2015-07-11 18:22:57 +0100

    Update demo_matplotlib.py

--- demo_matplotlib.py
+++ demo_matplotlib.py
@@ -1,8 +1,10 @@
 from vector3 import Vector3
 from tiling3_matplotlib import matplotlib_display_tiling3
-from tiling3_polyhedron import tetrahedron, octahedron
+from tiling3_polyhedron import tetrahedron, octahedron, cube, icosahedron, dodecahedron
+
 
 
 if __name__=="__main__":
-    matplotlib_display_tiling3(tiling3 = tetrahedron().translate(Vector3(0,0,0.00000001)))
-    matplotlib_display_tiling3(tiling3 = octahedron().translate(Vector3(0,0,0.00000001)))
+    platonic_solids = [tetrahedron(), cube(), octahedron(), dodecahedron(), icosahedron()]
+    for platonic_solid in platonic_solids:
+        matplotlib_display_tiling3(tiling3 = platonic_solid.translate(Vector3(0,0,0.0001))) 
