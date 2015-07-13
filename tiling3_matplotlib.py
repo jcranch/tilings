c983d4aa32fcdece38cc875d638a7e891e79f3a0
robertomath <robertonicolaides@hotmail.co.uk>
2015-07-13 14:52:48 +0100

    Deleted redundant function parameter.

--- tiling3_matplotlib.py
+++ tiling3_matplotlib.py
@@ -16,13 +16,13 @@ default_intersection_colours = ['orange','lime','red','aqua','magenta','darkgree
 
 
 
-def matplotlib_display_tiling3(tiling_3_on = True,intersection_tiling2_on = True,\
+def matplotlib_display_tiling3(tiling_3_on = True,axis_3D_intersection_tiling2_on = True,\
                                tiling3 = cubic_tiling3(((-1,4),(-1,1),(-1,1))).translate(Vector3(0,0,-0.000051)),\
                                intersection_colours = default_intersection_colours, intersection_alpha = 0.5,\
                                plane_z0_on = False ,plane_z0_alpha = 0.3,tiling3_colours = ['black'],plane_z0_colour = 'blue',\
                                tiling3_alpha = 0.5,initial_elevation = 20, initial_azimuth = 30, axis_3D_on = False,\
                                axis_3D_grid_on = False,\
-                               axis_3D_intersection_tiling2_on = True, save_on = True, save_name = 'demos/tiling3_figure'):
+                               save_on = True, save_name = 'demos/tiling3_figure'):
     figure = plt.figure()
     axis = Axes3D(figure)
     tiling2 = restrict32(tiling3)
