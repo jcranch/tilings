658ca9680693f453b59e0bd9f786d77adfe805cb
James Cranch <j.d.cranch@sheffield.ac.uk>
2015-07-14 14:07:47 +0100

    Idiomatic logical test

--- tiling2_polygon.py
+++ tiling2_polygon.py
@@ -16,7 +16,7 @@ def tiling2_polygon(vertices):
 
     
 def regular_polygon(n, radius=1.0, theta=0.0, centre=Vector2(0,0), grounded = False):
-    if grounded == True :
+    if grounded:
         theta = -(n-2)*pi/(2*n)
     v = [(i, centre + Vector2(radius*cos(2*pi*i/n + theta),
                               radius*sin(2*pi*i/n + theta)))
