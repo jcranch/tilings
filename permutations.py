0937f4c77494054366ae1df51b21f49cbcc8cb1b
robertomath <robertonicolaides@hotmail.co.uk>
2015-08-03 10:36:11 +0100

    New functions added to make regular polytopes easier to code.

--- permutations.py
+++ permutations.py
@@ -17,3 +17,37 @@ def even_permutations(original):
     for (s,p) in permutations_with_sign(original):
         if s == 1:
             yield p
+
+def all_unique_permutation_plus_minus(original):
+    permutables = []
+    for sign1 in [-1,1]:
+        for sign2 in [-1,1]:
+            for sign3 in [-1,1]:
+                for sign4 in [-1,1]:
+                    p = [original[0]*sign1,original[1]*sign2,original[2]*sign3,original[3]*sign4]
+                    if p not in permutables:
+                        permutables += [p]
+
+    vertices = []                    
+    for p in permutables:
+        for i in all_permutations(p):
+            if i not in vertices:
+                vertices += [i]
+    return vertices
+
+def only_even_unique_permutation_plus_minus(original):
+    permutables = []
+    for sign1 in [-1,1]:
+        for sign2 in [-1,1]:
+            for sign3 in [-1,1]:
+                for sign4 in [-1,1]:
+                    p = [original[0]*sign1,original[1]*sign2,original[2]*sign3,original[3]*sign4]
+                    if p not in permutables:
+                        permutables += [p]
+
+    vertices = []                    
+    for p in permutables:
+        for i in even_permutations(p):
+            if i not in vertices:
+                vertices += [i]
+    return vertices
