0bd19aa8e0f434689d8fa3270271068f387ed737
robertomath <robertonicolaides@hotmail.co.uk>
2015-09-01 12:34:22 +0100

    Added pictures for demo.

--- demo_matplotlib.py
+++ demo_matplotlib.py
@@ -2,6 +2,7 @@ from vector3 import Vector3
 from tiling3_matplotlib import matplotlib_display_tiling3
 from tiling3_polyhedron import tetrahedron, octahedron, cube, icosahedron, dodecahedron
 from tiling3_intersection_matplotlib import distribution_plot
+from tiling2_matplotlib import plot_matplotlib_multiple
 
 
 if __name__=="__main__":
@@ -9,3 +10,6 @@ if __name__=="__main__":
     for platonic_solid in platonic_solids:
         matplotlib_display_tiling3(tiling3 = platonic_solid.translate(Vector3(0,0,0.0001)))
         distribution_plot(platonic_solid)
+    #First nine regular polygon.
+    plot_matplotlib_multiple([regular_polygon(i, grounded= True).translate(Vector2(i*2.5 -4,0)) for i in range(3,10)],
+                         ticks_on = False, grid_on = False, userdefined_limits = [[2,20],[-2,2]] )
