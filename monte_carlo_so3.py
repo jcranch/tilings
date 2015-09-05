11c799f8ed65ecdb4d3ba68fe44a2a1c81805561
robertomath <robertonicolaides@hotmail.co.uk>
2015-09-05 12:24:31 +0100

    New and improved monte-carlo estimation!

--- monte_carlo_so3.py
+++ monte_carlo_so3.py
@@ -1,123 +1,50 @@
-from periodic_tiling3 import cubic_tiling3
+from tiling3_polyhedron import *
 from matrix3 import rotate_x, rotate_y,rotate_z, random_special_orthogonal
 from vector3 import Vector3
 from restrict32 import restrict32
 from common import cycle
 from tiling3 import Tiling3
 
-# Conceptually This function takes a tiling and rotates it using a uniformly, randomly selected rotation matrix in SO3
-# then counts polygons in the intersection of z = 0. This is designed to be repeated for many iterations.
+from random import random
 
-def montecarlo_so3(iterations, tiling3, transformation_function ):
-    # Take the tiling3, transform it wrt i and count the polygons on restrict32.
-    raw_n_gon_count_results = []
-    for i in range(iterations):
-        transforming_3d_tiling = transformation_function(tiling3,i)
-        transforming_2d_intersection = restrict32(transforming_3d_tiling).clip(-20,20,-20,20)
-        raw_n_gon_count_results += [transforming_2d_intersection.face_count_information()]
-    # Determine the unique n-gons that were found in the intersection.
-    distinct_n_gons_found = []
-    for dictionary_of_n_gons in raw_n_gon_count_results:
-        for n_gon in dictionary_of_n_gons:
-            if n_gon not in distinct_n_gons_found:
-                distinct_n_gons_found += [n_gon]
-    # Count how many of each n-gon were found overall.
-    n_gon_tally = dict([(n_gon,0) for n_gon in distinct_n_gons_found])
-    total_n_gons = 0.0
-    max_number_of_faces = 0.0
-    for i in range(iterations):
-        for n_gon in distinct_n_gons_found:
-            if n_gon in raw_n_gon_count_results[i]:
-                n_gon_tally[n_gon] += raw_n_gon_count_results[i][n_gon]
-                total_n_gons += raw_n_gon_count_results[i][n_gon]
-                if raw_n_gon_count_results[i][n_gon] > max_number_of_faces:
-                    max_number_of_faces = raw_n_gon_count_results[i][n_gon]
-    # Return the fraction of each.
-    n_gon_distribution = dict([(n_gon,n_gon_tally[n_gon]/total_n_gons) for n_gon in distinct_n_gons_found])
-    return n_gon_distribution
+def random_intersection(tiling3):
+    '''
+    Takes the original tiling3 object randomly rotates it about the origin.
+    Then it randomly translates the tiling3 such that it still intersects the plane z = 0.
+    '''
+    rotated_tiling3 = tiling3.deform(random_special_orthogonal())
+    bound = max([abs(tiling3.minx()),abs(tiling3.maxx()),
+                 abs(tiling3.miny()),abs(tiling3.maxy()),
+                 abs(tiling3.minz()),abs(tiling3.maxz())])
+    translation = Vector3(0,0,(2*random() - 1)*bound)
+    random_intersection = rotated_tiling3.translate(translation)
+    return random_intersection
 
 
-#Example:
-
-unit_cube = cubic_tiling3(((-0.2,1.2),(-0.2,1.2),(-0.2,1.2))).translate(Vector3(-0.50000001,-0.50000001,-0.50000001))
-iterations = 1000
-
-def produce_k_random_rotations_so3(k = iterations):
-    list_of_random_so3_rotations = []
-    for i in range(k):
-        list_of_random_so3_rotations += [random_special_orthogonal()]
-    return list_of_random_so3_rotations
-
-def deform_matrix(tiling3, i,list_of_matrices = produce_k_random_rotations_so3()):
-    return tiling3.transform(list_of_matrices[i])
-
-#Seems to tend to something like {4: 0.65,6:0.35}.
-#montecarlo_so3(iterations,tiling3 = unit_cube, transformation_function = deform_matrix)
-
-# Conceptually this function takes a 3d tiling, rotates randomly (as described before)
-# then translates it (preferably so that all the tiling is above z = 0)
-# and then slowly translates it frame by frame (preferably so that  all of the tiling is below z = 0)
-# whilst counting the n-gons produced with the intersection of z = 0.
-# It then returns the distribution of n-gons produced this way for all of randomly rotated 3d objects.
-def translation_function(tiling3,i):
-    return tiling3.translate(Vector3(0,0,-i/1000.0))
-
-
-def montecarlo_polygon_density(transformation_iterations=100,  tiling3 = unit_cube,\
-                               transformation_function = deform_matrix,translation_function = translation_function,\
-                               translation_count_safetylimit = 1000000):
-    grand_list_of_distinct_n_gons = []
-    grand_n_gon_tally = {}
-    grand_total_n_gons = 0.0
-    # Takes a tiling and rotates it.
-    for i in range(transformation_iterations):
-        transforming_3d_tiling = transformation_function(tiling3,i)
-        # If the 'lowest' point of the tiling lies below z = 0 then reaise it so that it lies just above.
-        if transforming_3d_tiling.minz() < 0:
-            transforming_3d_tiling = transforming_3d_tiling.translate(Vector3(0,0,-transforming_3d_tiling.minz()+0.000000001))
-        raw_n_gon_count_results = []
-        # Now we begin translating the given rotated tiling (preferably so that all of it lies below z = 0).
-        translation_count = 0
-        translating_3d_tiling = transforming_3d_tiling
-        # Translate it until 'highest' point in tiling is below z = 0 and record intersections of z = 0 along the way frame by frame.
-        while translating_3d_tiling.maxz() > 0 :
-            translating_3d_tiling = translation_function(transforming_3d_tiling,translation_count)
-            translating_2d_intersection = restrict32(translating_3d_tiling).clip(-20,20,-20,20)
-            # Record the n-gons that intercepted z = 0 this frame.
-            raw_n_gon_count_results += [translating_2d_intersection.face_count_information()]
-            translation_count += 1
-            if  translation_count > translation_count_safetylimit:
-                print "Translation count safetylimit has been surpassed."
-                break
-        # Find which n-gons were actually found. (I think I might be able to remove/improve this part!)
-        distinct_n_gons_found = []
-        for dictionary_of_n_gons in raw_n_gon_count_results:
-            for n_gon in dictionary_of_n_gons:
-                if n_gon not in distinct_n_gons_found:
-                    distinct_n_gons_found += [n_gon]
-                if n_gon not in grand_list_of_distinct_n_gons:
-                    grand_list_of_distinct_n_gons += [n_gon]
-        # Count how many of each n-gon were found overall.
-        for k in range(translation_count):
-            for n_gon in distinct_n_gons_found:
-                if n_gon in raw_n_gon_count_results[k]:
-                    grand_total_n_gons += raw_n_gon_count_results[k][n_gon]
-                    # Add results to our overall list
-                    try :
-                        grand_n_gon_tally[n_gon] += raw_n_gon_count_results[k][n_gon]
-                    except:
-                        grand_n_gon_tally[n_gon] = raw_n_gon_count_results[k][n_gon]
-    grand_n_gon_distribution = dict((n_gon,grand_n_gon_tally[n_gon]/grand_total_n_gons ) for n_gon in grand_list_of_distinct_n_gons)
-    return grand_n_gon_distribution
+def montecarlo_tiling3_cross_section_density(tiling3_s ,polygon_side_length_only_on = True):
+    '''
+    tiling3_s should be a list of tiling3 objects we wish to know the distribution of intersection
+    Returns the monte-carlo distribution of 2d cross-section of Tiling3 objects under certain tranformations.
+    polygon_sides_only_on determines whether or not to display how the n-gon was formed or the specific faces 
+    the edge is formed upon.
+    '''
+    raw_n_gon_count_results = {}
+    hits = 0.0
+    for tiling3 in tiling3_s:
+        restrict = restrict32(tiling3)
+        intersect_lines = frozenset(restrict.edges[e] for f in restrict.faces for e in f)
+        if polygon_side_length_only_on == True and intersect_lines:
+            intersect_lines = str(len(intersect_lines))+'-gons'
+        #print intersect_lines
+        try :
+            raw_n_gon_count_results[(intersect_lines)] += 1
+            hits += 1
+        except :
+            if intersect_lines:
+                raw_n_gon_count_results[(intersect_lines)] = 0.0
+                hits += 1
+    density_dictionary = dict([(intersect_lines,raw_n_gon_count_results[intersect_lines]/hits) for intersect_lines in raw_n_gon_count_results])
+    return density_dictionary
 
 
 
-'''
-For unit cube with 10000 transformation iterations I got this :
-montecarlo_polygon_density()
->>> {3: 0.2776192417448023,
- 4: 0.4866707210307887,
- 5: 0.18974424090942507,
- 6: 0.045965796314983993}
-'''
-montecarlo_polygon_density()
