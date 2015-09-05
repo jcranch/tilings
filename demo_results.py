9ae7d8ca5c52258d7167e05ccf7fccf0dcc52a1c
robertomath <robertonicolaides@hotmail.co.uk>
2015-09-05 12:42:26 +0100

    Corrected typo.

--- demo_results.py
+++ demo_results.py
@@ -7,7 +7,7 @@ Finds the distribution of random cross-section for the Platonic Solids.
 '''
 
 for polytope in regular_polytopes_3d:
-    print tiling3_polyhedron, montecarlo_tiling3_cross_section_density(
-    [random_intersection(regular_polytopes_3d[tiling3_polyhedron]) for i in range(100000)])
+    print polytope, montecarlo_tiling3_cross_section_density(
+    [random_intersection(regular_polytopes_3d[polytope]) for i in range(100000)])
     
 
