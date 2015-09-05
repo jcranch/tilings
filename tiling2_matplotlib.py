b6fcb02fb105a506bdbab5b75c27bae7e9bf3d26
robertomath <robertonicolaides@hotmail.co.uk>
2015-09-05 23:22:05 +0100

    New and improved tiling2 matplotlib display!

--- tiling2_matplotlib.py
+++ tiling2_matplotlib.py
@@ -2,75 +2,30 @@ import matplotlib.pyplot as plt
 from matplotlib.path import Path
 import matplotlib.patches as patches
 import numpy as np
-from matplotlib import animation
-
 from common import cycle
+from tiling3_matplotlib import default_intersection_colours
 
+import os
 
-default_colours = ['orange','lime','red','aqua','magenta','darkgreen','lightblue','gold','black','purple','blue','darkred','darkblue','lightgreen']
-
-
-def describe_polygon_path(polygon):
-    """
-    The polygon should be specified as a list of vertices of the form
-    [(a,b),(c,d)....(x,y)].
-    """
-    vertices = []
-    for vertex in polygon:
-        vertices.append(vertex)
-    vertices.append(polygon[0])
-    codes = [Path.MOVETO] + [Path.LINETO]*(len(polygon)-1) + [Path.CLOSEPOLY]
-    return Path(vertices, codes)
-
-
-def plot_matplotlib(tiling2, figure_size=8, grid_on=True,
-                    ticks_on=True, colours=default_colours, alpha=0.85):
-
-    """
-    Alpha determines translucency.
-    """
-
-    figure = plt.figure()
-    figure.set_size_inches(figure_size,figure_size)
-    axis = figure.add_subplot(111)
-    for (i,face) in enumerate(tiling2.faces):
-        l = describe_polygon_path([(v.x, v.y) for v in cycle(face)])
-        patch = patches.PathPatch(l, facecolor=colours[(len(face)-3)%len(colours)], lw=1.3, ec='k', alpha=alpha)
-        axis.add_patch(patch)
-    plt.axis('scaled')
-    axis.set_xlim(tiling2.minx()-1, tiling2.maxx()+1)
-    axis.set_ylim(tiling2.miny()-1, tiling2.maxy()+1)
-    if grid_on == True :
-        axis.grid(True)
-    if ticks_on == False:
-        axis.set_xticks([])
-        axis.set_yticks([])
-    return figure
-
-def plot_matplotlib_multiple(tiling2_s, figure_size=8, grid_on=True,
-                    ticks_on=True, colours=default_colours, alpha=0.85, userdefined_limits = False):
-
-    """
-    The tiling2_s argument should be a list of tiling2 objects.
-    """
-
-    figure = plt.figure(frameon = True)
-    figure.set_size_inches(figure_size,figure_size)
-    axis = figure.add_subplot(111)
+def tiling2_s_flattened_subplot(tiling2_s, figure = False, position_code = 111, colours = default_intersection_colours,
+                                xlimits = [-5,5], ylimits = [-5,5], save_name = 'tiling2_image', folder = 'demos/tiling2',
+                               save_on = True):
+    if figure == False :
+        figure = plt.figure()
+    folder_name = os.path.join(folder, save_name+"_png")
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
+    axis = plt.subplot(position_code, xlim = xlimits, ylim = ylimits, aspect='equal', frameon = False)
+    axis.get_xaxis().set_visible(False)
+    axis.get_yaxis().set_visible(False)
+    patches = []
+    grand_n = 0
     for tiling2 in tiling2_s:
-        for (i,face) in enumerate(tiling2.faces):
-            l = describe_polygon_path([(v.x, v.y) for v in cycle(face)])
-            patch = patches.PathPatch(l, facecolor=colours[(len(face)-3)%len(colours)], lw=1.3, ec='k', alpha=alpha)
-            axis.add_patch(patch)
-    plt.axis('scaled')
-    if userdefined_limits != False:
-        axis.set_xlim(-userdefined_limits[0][0],userdefined_limits[0][1])
-        axis.set_ylim(-userdefined_limits[1][0],userdefined_limits[1][1])
-    axis.axis()
-    if grid_on == True :
-        axis.grid(True)
-    if ticks_on == False:
-        axis.set_xticks([])
-        axis.set_yticks([])
-    return figure
-
+        for (n,face) in enumerate(tiling2.faces):
+            patches.append(axis.add_patch(plt.Polygon(0 * np.array([(v.x, v.y) for v in cycle(face)]),
+            facecolor = colours[(len(face)-3)%len(colours)] , ec='k', alpha = 0.6)))
+            patches[grand_n+n].set_xy(np.array([(v.x, v.y) for v in cycle(face)]))
+        grand_n += 1
+    if save_on == True:
+        figure.savefig(os.path.join(folder_name, str(save_name)))         
+    return axis
