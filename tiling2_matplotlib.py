01e641460c93ba359d26d497761e893246a0078d
robertomath <robertonicolaides@hotmail.co.uk>
2015-09-06 14:38:28 +0100

    Update tiling2_matplotlib.py

--- tiling2_matplotlib.py
+++ tiling2_matplotlib.py
@@ -1,15 +1,15 @@
+from common import cycle
+from tiling3_matplotlib import default_intersection_colours
+
 import matplotlib.pyplot as plt
 from matplotlib.path import Path
 import matplotlib.patches as patches
 import numpy as np
-from common import cycle
-from tiling3_matplotlib import default_intersection_colours
-
 import os
 
 def tiling2_s_flattened_subplot(tiling2_s, figure = False, position_code = 111, colours = default_intersection_colours,
                                 xlimits = [-5,5], ylimits = [-5,5], save_name = 'tiling2_image', folder = 'demos/tiling2',
-                               save_on = True):
+                               save_on = True, tiling2_alpha = 0.8):
     if figure == False :
         figure = plt.figure()
     folder_name = os.path.join(folder, save_name+"_png")
@@ -23,7 +23,7 @@ def tiling2_s_flattened_subplot(tiling2_s, figure = False, position_code = 111, 
     for tiling2 in tiling2_s:
         for (n,face) in enumerate(tiling2.faces):
             patches.append(axis.add_patch(plt.Polygon(0 * np.array([(v.x, v.y) for v in cycle(face)]),
-            facecolor = colours[(len(face)-3)%len(colours)] , ec='k', alpha = 0.6)))
+            facecolor = colours[(len(face)-3)%len(colours)] , ec='k', alpha = tiling2_alpha)))
             patches[grand_n+n].set_xy(np.array([(v.x, v.y) for v in cycle(face)]))
         grand_n += 1
     if save_on == True:
