d1e1c5eaff8b3d850b2309bcd2091b5f2d43de1a
James Cranch <cranch@cantab.net>
2015-09-06 13:00:16 +0100

    Getting demo_results to work for me.

--- demo_results.py
+++ demo_results.py
@@ -6,8 +6,7 @@ from tiling3_polyhedron import regular_polytopes_3d
 Finds the distribution of random cross-section for the Platonic Solids.
 '''
 
-for polytope in regular_polytopes_3d:
-    print polytope, montecarlo_tiling3_cross_section_density(
-    [random_intersection(regular_polytopes_3d[polytope]) for i in range(100000)]), , '\n'
+for (name, polytope) in regular_polytopes_3d.iteritems():
+    print name, montecarlo_tiling3_cross_section_density([random_intersection(polytope) for i in range(100000)]), '\n'
     
 
