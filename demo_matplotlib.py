ba3a25d93930c399ea3bcdd3ae9a4ecffe1c0921
robertomath <robertonicolaides@hotmail.co.uk>
2015-09-17 19:16:47 +0100

    Update demo_matplotlib.py

--- demo_matplotlib.py
+++ demo_matplotlib.py
@@ -1,16 +1,15 @@
 from math import pi
+import matplotlib.pyplot as plt
 
 from vector2 import Vector2
 from vector3 import Vector3
 from matrix3 import rotate_x, rotate_y, rotate_z
-from tiling3_matplotlib import tiling3_s_3d_subplot
 from tiling3_polyhedron import tetrahedron, octahedron, cube, icosahedron, dodecahedron
 from matplotlib_imagemaker import Tiling3ImageMaker, Tiling2ImageMaker
 from tiling2_polygon import regular_polygon
 
-tiling2_image_creator = Tiling2ImageMaker()
 tiling3_image_creator = Tiling3ImageMaker()
 if __name__=="__main__":
     platonic_solids = [tetrahedron(), cube(), octahedron(), dodecahedron(), icosahedron()]
-    for platonic_solid in platonic_solids:
-        tiling3_image_creator.store_image([platonic_solid.translate(Vector3(0,0,0.0001))])
+    for (i,platonic_solid) in platonic_solids:
+        tiling3_image_creator.store_image([platonic_solid.translate(Vector3(0,0,0.0001))], folder = 'demos/platonic_solids')
