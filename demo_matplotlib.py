45d9654ed65fa3ca14caee1e66481caa10eea034
robertomath <robertonicolaides@hotmail.co.uk>
2015-09-17 18:33:43 +0100

    Update demo_matplotlib.py

--- demo_matplotlib.py
+++ demo_matplotlib.py
@@ -5,13 +5,12 @@ from vector3 import Vector3
 from matrix3 import rotate_x, rotate_y, rotate_z
 from tiling3_matplotlib import tiling3_s_3d_subplot
 from tiling3_polyhedron import tetrahedron, octahedron, cube, icosahedron, dodecahedron
-from tiling3_intersection_matplotlib import distribution_plot
-from tiling2_matplotlib import plot_matplotlib_multiple
+from matplotlib_imagemaker import Tiling3ImageMaker, Tiling2ImageMaker
 from tiling2_polygon import regular_polygon
 
-
+tiling2_image_creator = Tiling2ImageMaker()
+tiling3_image_creator = Tiling3ImageMaker()
 if __name__=="__main__":
     platonic_solids = [tetrahedron(), cube(), octahedron(), dodecahedron(), icosahedron()]
     for platonic_solid in platonic_solids:
-        tiling3_s_3d_subplot([platonic_solid.translate(Vector3(0,0,0.0001))])
-        distribution_plot(platonic_solid)
+        tiling3_image_creator.store_image([platonic_solid.translate(Vector3(0,0,0.0001))])
